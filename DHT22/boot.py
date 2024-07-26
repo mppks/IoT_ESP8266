cd81dfe22e2094147d2de8a08efece607b891afe
Konstantin Pavlov <pavlovks@gmail.com>
2024-07-27 00:29:43 +0300

    refactor(DHT22/boot.py): Move the code responsible for networks into functions

--- DHT22/boot.py
+++ DHT22/boot.py
@@ -1,48 +1,42 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-debug = True
-
 import config
+import machine, network, esp, os, gc, webrepl
 
 try:
     import usocket as socket
 except:
     import socket
 
-import esp
-if debug:
-    esp.osdebug(None)
-
-import os, machine
+debug = True
 if not debug:
-    os.dupterm(None, 1) # disable REPL on UART(0)
+    esp.osdebug(None)
+    os.dupterm(None, 1)  # disable REPL on UART(0)
     
-import gc
+
+def connect_sta():
+    """ Connect to LAN """
+    sta = network.WLAN(network.STA_IF)
+    sta.active(True)
+    if not sta.isconnected():
+        sta.config(dhcp_hostname=config.STA_HOSTNAME)
+        sta.connect(config.STA_SSID, config.STA_PASSWORD)
+        while not sta.isconnected():
+            pass
+    if debug:
+        print(sta.ifconfig())
+
+
+def create_ap():
+    """ Create Access point """
+    ap = network.WLAN(network.AP_IF)
+    ap.active(True)
+    ap.config(essid=config.AP_SSID, password=config.AP_PASSWORD, authmode=network.AUTH_WPA_WPA2_PSK)
+    while not ap.active():
+        pass
+    if debug:
+        print(ap.ifconfig())
+
 gc.collect()
-
-# Connect to LAN
-import network
-sta = network.WLAN(network.STA_IF)
-sta.active(True)
-sta.config(dhcp_hostname = config.STA_HOSTNAME)
-sta.connect(config.STA_SSID, config.STA_PASSWORD)
-
-while sta.isconnected() == False:
-    pass
-
-if debug:
-    print(sta.ifconfig())
-
-# Create Access point
-ap = network.WLAN(network.AP_IF)
-ap.active(True)
-ap.config(essid = config.AP_SSID, password = config.AP_PASSWORD, authmode = network.AUTH_WPA_WPA2_PSK)
-
-while ap.active() == False:
-    pass
-
-if debug:
-    print(ap.ifconfig())
-
-# Start WebREPL
-import webrepl
+connect_sta()
+create_ap()
 webrepl.start()
